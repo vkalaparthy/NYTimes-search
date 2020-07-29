f2b2c3316a73242b1e073172b01985aa7abd5f9e
vkalaparthy <kalaparthy.vani@gmail.com>
2020-07-29 16:20:26 -0400

    Added more inputs

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ $(".btn-primary").css({"background-color": "#000099"});
 
 var searchT = "";
 var searchNum = 1;
-var searchyear = 2020;
+var searchstart = 2020;
+var searchEnd = 0;
 //var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
 
 $("#search-button").on("click", function() {
@@ -19,22 +20,27 @@ $("#search-button").on("click", function() {
     console.log(queryURL);
     searchNum = $("#searchNum").val();
     console.log(searchNum);
-    searchyear = $("#searchYear").val();
-    console.log(searchyear);
+    searchstart = $("#searchStart").val();
+    console.log(searchstart);
+    if (searchstart != "")
+      queryURL = queryURL + "&begin_date="+searchstart;
+    searchend = $("#searchEnd").val();
+    if (searchend != "")
+      queryURL = queryURL + "&end_date="+searchend;
     queryURL = queryURL + "&api-key=HChE63INGzA71ycGg6rH9ZIoqRlyNevj";
     console.log(queryURL);
-    searchArticles(queryURL);
+    searchArticles(searchNum, queryURL);
   });
 
 //queryURL = queryURL + "&api-key=HChE63INGzA71ycGg6rH9ZIoqRlyNevj";
 
-  function searchArticles(queryURL) {
+  function searchArticles(numCount, queryURL) {
     $.ajax({
       url: queryURL,
       method: "GET"
     }). then (function(data) {
       console.log(data.response.docs);
-      for (var i = 0; i < data.response.docs.length; i++){
+      for (var i = 0; i < numCount; i++){
         var newCard = $("<div>").addClass("card");
         var newCardBody = $("<div>").addClass("card-body");
         var newP1 = $("<p>").text(i+1 + " " + data.response.docs[i].abstract);
@@ -45,7 +51,8 @@ $("#search-button").on("click", function() {
           sectText = data.response.docs[i].section_name;
         }
         var newP3 = $("<p>").text("Section: " + sectText);
-        newCardBody.append(newP1, newP2, newP3);
+        var newP4 = $("<p>").text("Published On: " + data.response.docs[i].pub_date);
+        newCardBody.append(newP1, newP2, newP3, newP4);
         newCard.append(newCardBody);
         $("#articles").append(newCard);
       }
